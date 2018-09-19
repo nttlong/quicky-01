42e5870da637d426594e3f89e44e9d2bbdd02b50
nttlong <zugeliang2000@gamil.com>
2018-09-19 20:50:49 +0700

    qmongo AGGREGATE save as view support

---
+++ packages/pkg_qexcel/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import qexcel
+setup(
+    name='qexcel',
+    version=qexcel.get_version(),
+    packages=['qexcel'],
+    url='',
+    license='MIT',
+    author='nttlong',
+    author_email='zugeliang2000@gmail.com',
+    description=''
+)
