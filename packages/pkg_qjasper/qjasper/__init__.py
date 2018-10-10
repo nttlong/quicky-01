c4bb0f4712f5e24d71692c8369ff4c0e4dfd8bd4
nttlong <zugeliang2000@gmail.com>
2018-10-10 14:27:14 +0700

    allow jasper client auto load config from django settings

--- packages/pkg_qjasper/qjasper/__init__.py
+++ packages/pkg_qjasper/qjasper/__init__.py
@@ -1,5 +1,5 @@
 # encoding=utf8
-VERSION = [1,0,0,"beta",5]
+VERSION = [1,0,0,"beta",6]
 from . import search_filter
 def get_version():
     return VERSION[0].__str__()+\
@@ -22,11 +22,28 @@ def set_config(*args,**kwargs):
         params=kwargs
     global __config
     __config=params
+def load_config_from_django():
+    from django.conf import settings
+    try:
+        jasper_config = settings.JASPER
+        set_config(jasper_config)
+    except Exception as ex:
+        raise Exception("It looks like you forgot set config for jasper report in django settings\n"
+                        "\tHow to set config for jasper?\n"
+                        "\t\t In django setting declare bellow :\n"
+                        "\t\tJASPER=dict(\n"
+                        "\t\t\tURL='<http|https>://<host>:<port>/jasperserver',\n"
+                        "\t\t\tUSER='<username>,'" \
+                        "\t\t\tPASSWORD='jasperadmin'")
 def get_config():
     global __config
+    if __config== None:
+        load_config_from_django()
     return __config
 def get_url_server():
     global __config
+    if __config== None:
+        load_config_from_django()
     if __config == None or not __config.has_key("url"):
         raise (Exception("It look likes you forgot call set_config of qjasper\n"
                          "How to use qjasper:\n"
@@ -34,6 +51,8 @@ def get_url_server():
                          "\t\t"))
     return __config["url"]
 def get_auth():
+    if __config== None:
+        load_config_from_django()
     return (__config["user"],__config["password"])
 from . search_filter import resource_types,resource_sort_fields
 def filter(name_or_description=None,
