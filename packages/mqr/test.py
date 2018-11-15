544140540d2874b31c98bbd01bd58d3dc053c3c1
nttlong <zugeliang2000@gmail.com>
2018-11-15 18:07:38 +0700

    save

--- packages/mqr/test.py
+++ packages/mqr/test.py
@@ -1,6 +1,6 @@
 import compilers
 import expression_parser
-x=compilers.compile_expression("c.a.d=15")
+x=expression_parser.to_mongodb_expr("x=={0}",'aaa')
 # expr,params=expression_parser.parse("$x==@test1",test1=2,username='admin')
 print x
 #print params
